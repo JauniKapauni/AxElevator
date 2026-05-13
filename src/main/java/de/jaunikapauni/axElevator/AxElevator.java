6398478aafdb18da3104a9e976ad41dd60efd970
JauniKapauni <riches-goods.54@icloud.com>
2026-05-13 17:11:20 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axElevator/AxElevator.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axElevator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxElevator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
